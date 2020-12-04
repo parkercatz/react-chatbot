045852392367e87390a3a85e78a794a67aacdf35
ParkerCatz <ParkerCatz@ParkerCatz.local>
2020-12-04 16:49:00 +0900

    Add: お問い合わせモーダルの追加 -------------------------------- ・モーダルコンポーネントをMaterial-UIで作成 ・問い合わせるを押下時にモーダルが出力する ・モーダル内のボタンを押下時にcloseする（一旦）

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './assets/styles/style.css'
 import defaultDataset from './dataset'
 import { AnswersList, Chats } from './components/index'
+import FormDialog from './components/Forms/FormDialog'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +15,8 @@ export default class App extends React.Component {
       open: false,
     }
     this.selectAnswer = this.selectAnswer.bind(this)
+    this.handleClickOpen = this.handleClickOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -34,6 +37,9 @@ export default class App extends React.Component {
     switch (true) {
       case nextQuestionId === 'init':
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500)
+        break
+      case nextQuestionId === 'contact':
+        this.handleClickOpen()
         break
       case /^https:*/.test(nextQuestionId):
         const a = document.createElement('a')
@@ -66,6 +72,14 @@ export default class App extends React.Component {
     })
   }
 
+  handleClickOpen = () => {
+    this.setState({ open: true })
+  }
+
+  handleClose = () => {
+    this.setState({ open: false })
+  }
+
   componentDidMount() {
     const initAnswer = ''
     this.selectAnswer(initAnswer, this.state.currentId)
@@ -88,6 +102,7 @@ export default class App extends React.Component {
               answers={this.state.answers}
               select={this.selectAnswer}
             />
+            <FormDialog open={this.state.open} handleClose={this.handleClose} />
           </div>
         </section>
       </div>
