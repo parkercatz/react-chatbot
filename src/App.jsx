8c65d014c181ee5a88f9f95c467a7e7353ccb173
ParkerCatz <ParkerCatz@ParkerCatz.local>
2020-12-05 09:59:32 +0900

    Fix: リファクタリング -------------------------------- ・slackのWebhookURLをgithub管理対象外に変更 ・Formsのコンポーネントをエントリーポイント化 ・useCallbackの適用 ・FormDialogのinput部分の修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import './assets/styles/style.css'
 import { AnswersList, Chats } from './components/index'
-import FormDialog from './components/Forms/FormDialog'
+import { FormDialog } from './components/Forms/index'
 import { db } from './firebase/index'
 
 const App = () => {
-  const [answers, setAnswers] = useState([])
-  const [chats, setChats] = useState([])
-  const [currentId, setCurrentId] = useState('init')
-  const [dataset, setDataset] = useState({})
-  const [open, setOpen] = useState(false)
+  const [answers, setAnswers] = useState([]) // 回答コンポーネントに表示するデータ
+  const [chats, setChats] = useState([]) // チャットコンポーネントに表示するデータ
+  const [currentId, setCurrentId] = useState('init') // 現在の質問ID
+  const [dataset, setDataset] = useState({}) // 質問と回答のデータセット
+  const [open, setOpen] = useState(false) // お問い合わせフォーム用モーダルの開閉を管理
 
+  // 次の質問をチャットエリアに表示する関数
   const displayNextQuestion = (nextQuestionId, nextDataset) => {
     addChats({
       text: nextDataset.question,
@@ -20,6 +21,7 @@ const App = () => {
     setCurrentId(nextQuestionId)
   }
 
+  // 回答が選択された時に呼ばれる関数
   const selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch (true) {
       case nextQuestionId === 'contact':
@@ -46,23 +48,32 @@ const App = () => {
     }
   }
 
-  const addChats = (chat) => {
-    setChats((prevChats) => {
-      return [...prevChats, chat]
-    })
-  }
+  // 新しいチャットを追加するCallback関数
+  const addChats = useCallback(
+    (chat) => {
+      setChats((prevChats) => {
+        return [...prevChats, chat]
+      })
+    },
+    [setChats]
+  )
 
-  const handleOpen = () => {
+  // お問い合わせフォーム用モーダルを開くCallback関数
+  const handleOpen = useCallback(() => {
     setOpen(true)
-  }
+  }, [setOpen])
 
+  // お問い合わせフォーム用モーダルを閉じるCallback関数
   const handleClose = useCallback(() => {
     setOpen(false)
   }, [setOpen])
 
+  // 最初の質問をチャットエリアに表示する
   useEffect(() => {
     ;(async () => {
       const initDataset = {}
+
+      // FireStoreから'question'を取得する
       await db
         .collection('questions')
         .get()
@@ -74,11 +85,15 @@ const App = () => {
           })
         })
 
+      // FireStoreから取得したデータセットを反映
       setDataset(initDataset)
+
+      // 最初の質問を表示
       displayNextQuestion(currentId, initDataset[currentId])
     })()
   }, [])
 
+  // 最初のチャットが見えるように、スクロール位置の頂点をスクロール領域の最下部に設定する
   useEffect(() => {
     const scrollArea = document.getElementById('scroll-area')
     if (scrollArea) {
--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -1,28 +1,39 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useCallback } from 'react'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import TextInput from './TextInput'
+import { TextInput } from './index'
+import { WEBHOOK_URL } from '../../webhookConfig'
 
 const FormDialog = (props) => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [description, setDescription] = useState('')
 
-  const inputName = useCallback((event) => {
-    setName({ name: event.target.value }, [setName])
-  })
+  const inputName = useCallback(
+    (event) => {
+      setName(event.target.value)
+    },
+    [setName]
+  )
 
-  const inputEmail = useCallback((event) => {
-    setEmail({ email: event.target.value }, [setEmail])
-  })
+  const inputEmail = useCallback(
+    (event) => {
+      setEmail(event.target.value)
+    },
+    [setName]
+  )
 
-  const inputDescription = useCallback((event) => {
-    setDescription({ description: event.target.value }, [setDescription])
-  })
+  const inputDescription = useCallback(
+    (event) => {
+      setDescription(event.target.value)
+    },
+    [setDescription]
+  )
 
+  // slackにお問い合わせがあったことを通知する
   const submitForm = () => {
     const payload = {
       text:
@@ -37,17 +48,15 @@ const FormDialog = (props) => {
         description,
     }
 
-    const url =
-      'https://hooks.slack.com/services/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'
-
-    fetch(url, {
+    // フォームの内容をslackのIncoming Webhook URLに送信する
+    fetch(WEBHOOK_URL, {
       method: 'POST',
       body: JSON.stringify(payload),
     }).then(() => {
-      alert('送信が完了しました。追って連絡します！')
-      setName('')
-      setEmail('')
+      alert('送信が完了しました。追ってご連絡いたします🙌')
       setDescription('')
+      setEmail('')
+      setName('')
       return props.handleClose()
     })
   }
@@ -90,7 +99,7 @@ const FormDialog = (props) => {
         <Button onClick={props.handleClose} color="primary">
           キャンセル
         </Button>
-        <Button onClick={submitForm} color="primary" autoFocus>
+        <Button onClick={submitForm} color="primary">
           送信する
         </Button>
       </DialogActions>
